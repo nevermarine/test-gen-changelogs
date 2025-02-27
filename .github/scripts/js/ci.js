b1f82edbc98b60edd226648f5a230d4a2f9e13cb
Maksim Fedotov <maksim.fedotov@flant.com>
2025-02-27 11:23:38 +0300

    add files for e2e runs
    Signed-off-by: Maksim Fedotov <maksim.fedotov@flant.com>

--- .github/scripts/js/ci.js
+++ .github/scripts/js/ci.js
@@ -10,7 +10,13 @@
 // limitations under the License.
 
 //@ts-check
+//
+const {
+  knownLabels,
+  skipE2eLabel
+} = require('./constants');
 
+const e2eStatus = require('./e2e-commit-status');
 /* 
  * this file contains only 3 functions from the original ci.js: 
  * extractCommandFromComment, reactToComment, startWorkflow.
@@ -103,3 +109,181 @@ const startWorkflow = async ({ github, context, core, workflow_id, ref, inputs }
 	return core.info(`Workflow '${workflow_id}' started successfully`);
 };
 module.exports.startWorkflow = startWorkflow;
+
+/**
+ * Get labels from PR and determine a workflow to run next.
+ *
+ * @param {object} inputs
+ * @param {object} inputs.github - A pre-authenticated octokit/rest.js client with pagination plugins.
+ * @param {object} inputs.context - An object containing the context of the workflow run.
+ * @param {object} inputs.core - A reference to the '@actions/core' package.
+ * @param {string} inputs.ref - A git ref to checkout head commit for PR (e.g. refs/pull/133/head).
+ * @returns {Promise<void>}
+ */
+module.exports.runWorkflowForPullRequest = async ({ github, context, core, ref }) => {
+  const event = context.payload;
+  const label = event.label.name;
+  const prNumber = context.payload.pull_request.number;
+  const prLabels = context.payload.pull_request.labels;
+
+  core.startGroup(`Dump context`);
+  core.info(`Git ref for workflows: ${ref}`);
+  core.info(`PR number: ${prNumber}`);
+  core.info(`PR action: ${event.action}`);
+  core.info(`PR action label: '${label}'`);
+  core.info(
+    `Current labels: ${JSON.stringify(
+      prLabels.map((l) => l.name),
+      null,
+      '  '
+    )}`
+  );
+  core.info(`Known labels: ${JSON.stringify(knownLabels, null, '  ')}`);
+  core.endGroup();
+
+  // Note: no more auto rerun for validation.yml.
+
+  let command = {
+    // null - do nothing
+    // true - pr labeled
+    // false- pr unlabeled
+    setE2eShouldSkipped: null,
+    rerunWorkflow: false,
+    triggerWorkflowDispatch: false,
+    workflows: []
+  };
+  core.startGroup(`PR#${prNumber} was ${event.action} with '${label}'. Detect command ...`);
+  try {
+    const labelInfo = knownLabels[label];
+    const labelType = labelInfo ? labelInfo.type : '';
+    if (label === skipE2eLabel) {
+      // set commit status
+      command.setE2eShouldSkipped = event.action === 'labeled';
+    }
+    if (labelType === 'e2e-run' && event.action === 'labeled') {
+      // Workflow will remove label from PR, ignore 'unlabeled' action.
+      command.workflows = [`run-e2e-on-user-cluster.yml`];
+      command.triggerWorkflowDispatch = true;
+    }
+    if (labelType === 'deploy-web' && event.action === 'labeled') {
+      // Workflow will remove label from PR, ignore 'unlabeled' action.
+      command.workflows = [`deploy-web-${labelInfo.env}.yml`];
+      command.triggerWorkflowDispatch = true;
+    }
+    if (labelType === 'ok-to-test') {
+      command.workflows = ['build-and-test_dev.yml', 'validation.yml'];
+      command.rerunWorkflow = true;
+    }
+    // Rerun build workflow if edition label is added or all edition labels are removed.
+    if (labelType === 'edition') {
+      // Gather other edition labels on PR.
+      let removeEditions = [];
+      prLabels.map((l) => {
+        const info = knownLabels[l.name];
+        if (info && info.type === 'edition' && l.name !== label) {
+          removeEditions.push(l.name);
+        }
+      });
+
+      if (event.action === 'labeled' && removeEditions.length > 0) {
+        // If edition/ce label is set, edition/ee label should be removed and vice versa.
+        for (const edition of removeEditions) {
+          core.notice(`Remove label '${edition}' from PR#${prNumber}`);
+          await removeLabel({ github, context, core, issue_number, label: edition });
+        }
+      }
+
+      // Re-run workflow if labeled with edition label or no edition labels left on PR.
+      if (event.action === 'labeled' || (event.action === 'unlabeled' && removeEditions.length === 0)) {
+        command.workflows = ['build-and-test_dev.yml'];
+        command.rerunWorkflow = true;
+      }
+    }
+    if (labelType === 'security') {
+      if (labelInfo.security === 'rootless' && event.action === 'labeled') {
+        command.workflows = ['build-and-test_dev.yml'];
+        command.rerunWorkflow = true;
+      }
+    }
+  } finally {
+    core.endGroup();
+  }
+
+  if (command.setE2eShouldSkipped !== null) {
+    return e2eStatus.onLabeledForSkip({
+      github,
+      context,
+      core,
+      labeled: command.setE2eShouldSkipped,
+      commitSha: context.payload.pull_request.head.sha
+    });
+  }
+
+  if (command.workflows.length === 0) {
+    return core.notice(`Ignore '${event.action}' event for label '${label}': no workflow to rerun.`);
+  }
+
+  if (command.rerunWorkflow) {
+    core.notice(`Retry workflows '${JSON.stringify(command.workflows)}' for label '${label}'`);
+    for (const workflow_id of command.workflows) {
+      await findAndRerunWorkflow({ github, context, core, workflow_id });
+    }
+  }
+
+  if (command.triggerWorkflowDispatch) {
+    // Can trigger only single workflow because of commenting on PR.
+    const workflow_id = command.workflows[0];
+    core.notice(`Run workflow '${JSON.stringify(command.workflows)}' for label '${label}'`);
+    core.startGroup(`Trigger workflow_dispatch event ...`);
+    try {
+      // Add a comment to pull request. https://docs.github.com/en/rest/issues/comments#create-an-issue-comment
+      core.info(`Commenting on PR#${prNumber} ...`);
+      const response = await github.rest.issues.createComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: prNumber,
+        body: commentLabelRecognition(context.payload.sender.login, label)
+      });
+
+      if (response.status !== 201) {
+        return core.setFailed(`Error commenting PR#${prNumber}: ${JSON.stringify(response)}`);
+      }
+
+      const commentInfo = {
+        issue_id: '' + context.payload.pull_request.id,
+        issue_number: '' + prNumber,
+        comment_id: '' + response.data.id
+      };
+
+      // Triggering workflow_dispatch requires a ref to checkout workflows.
+      // We use refs/heads/main for workflows and pass refs/pulls/head/NUM in
+      // pull_request_ref field to checkout PR content.
+      const targetRepo = context.payload.repository.full_name;
+      const prRepo = context.payload.pull_request.head.repo.full_name;
+      const prRef = context.payload.pull_request.head.ref;
+      const prInfo = {
+        ci_commit_ref_name: prRepo === targetRepo ? prRef : `pr${prNumber}`,
+        pull_request_ref: ref,
+        pull_request_sha: context.payload.pull_request.head.sha,
+        pull_request_head_label: context.payload.pull_request.head.label
+      };
+
+      core.debug(`Pull request info: ${JSON.stringify(prInfo)}`);
+      await startWorkflow({
+        github,
+        context,
+        core,
+        workflow_id,
+        ref: 'refs/heads/main',
+        inputs: {
+          ...commentInfo,
+          ...prInfo
+        }
+      });
+    } catch (error) {
+      core.info(`Github API call error: ${dumpError(error)}; (${JSON.stringify(error)})`);
+    } finally {
+      core.endGroup();
+    }
+  }
+}; 
